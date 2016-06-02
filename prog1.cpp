e5f1f28afacabc9bdf5408846e913695e62d9cad
amrutak25 <amrutak25@gmail.com>
2016-06-01 23:35:34 -0700

    Update prog1.cpp

--- prog1.cpp
+++ prog1.cpp
@@ -12,16 +12,19 @@ int data[N];
 for (int i=0; i<N ; i++)
     data[i] =i;
     
-sum(&accum, N, data);
+for (i=0; i<N ; i++)
+   accum+= d[i];
+
+       //sum(&accum, N, data);
 cout << "sum is %d\n" << accum ;
 return 0;
 
 }
 
-void sum (int *p, int n, int d[])
-{
-
-for(int i=0; i<N; i++)
-   *p = *p +d[i];
-
-}
+//void sum (int *p, int n, int d[])
+//{
+//
+//for(int i=0; i<N; i++)
+//   *p = *p +d[i];
+//
+//}
